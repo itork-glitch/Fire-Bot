9b3b9c55d8604c05561de8f2e580f359f786ca71
itork-glitch <piter09102009sx@gmail.com>
2023-03-04 13:23:02 +0100

    version 8.1

--- main.js
+++ main.js
@@ -5,13 +5,10 @@ const {
   Collection,
 } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
-
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
-
 const { loadEvents } = require('./Handlers/eventHandler');
 const { loadCommands } = require('./Handlers/commandHandler');
-
 const client = new Client({
   intents: [Guilds, GuildMembers, GuildMessages],
   partials: [User, Message, GuildMember, ThreadMember],
@@ -21,8 +18,6 @@ client.config = require('./security/config.json');
 const key = require('./security/key.json');
 
 const SpotifyWebApi = require('spotify-web-api-node');
-
-// Tworzymy instancję klienta Spotify
 const spotifyApi = new SpotifyWebApi({
   clientId: key.spotifyID,
   clientSecret: key.spotifySecret,
@@ -49,7 +44,28 @@ client.once(`ready`, () => {
   }, 5000); // 1 s = 1000 ms
 
   client.user.setStatus(`online`); // dnd, idle, online, invisible
+
+  try {
+    getSpotifyToken();
+    setInterval(() => {
+      getSpotifyToken();
+    }, 900000); // co 15 minut (900000 ms)
+    console.log(' Połączono z spotify');
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+async function getSpotifyToken() {
+  try {
+    const data = await spotifyApi.clientCredentialsGrant();
+    const token = data.body.access_token;
+    spotifyApi.setAccessToken(token);
+  } catch (err) {
+    console.log('Błąd podczas pobierania tokenu dostępu Spotify.', err.message);
+  }
+}
+
 client
   .login(key.token)
   .then(() => {
